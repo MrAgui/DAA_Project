32fc03efd194b38d9ca4fc756e6b6cb82bd1d96a
MrAgui <karlaguilar3800@gmail.com>
2022-04-09 20:31:16 +0800

    Fix the mistake in splashActivity

--- app/src/main/java/com/bsu/JustDoIt/SplashActivity.java
+++ app/src/main/java/com/bsu/JustDoIt/SplashActivity.java
@@ -1,6 +1,14 @@
 package com.bsu.JustDoIt;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashActivity extends Activity {
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
