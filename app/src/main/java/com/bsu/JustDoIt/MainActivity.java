8eec31e36f3c29ee5628ca99c74f7f1e1a792a64
MrAgui <karlaguilar3800@gmail.com>
2022-04-09 21:06:16 +0800

    Adde ToDoModel Class for the variables Getters and Setters

--- app/src/main/java/com/bsu/JustDoIt/MainActivity.java
+++ app/src/main/java/com/bsu/JustDoIt/MainActivity.java
@@ -1,15 +1,24 @@
 package com.bsu.JustDoIt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView tasksRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
 }
---
+++ app/src/main/java/com/bsu/JustDoIt/Model/ToDoModel.java
@@ -0,0 +1,31 @@
+package com.bsu.JustDoIt.Model;
+
+public class ToDoModel {
+    // Status is a boolean variable but we use int 1 for true and 0 for false because of the database Issues
+    private int id, status;
+    private String task;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
